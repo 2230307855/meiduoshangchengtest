cc71117cae1f22cca0c86db2236a9e02426c6e24
Chenglong Shi <2230307855@qq.com>
2021-11-01 22:26:51 +0800

    视图的处理

--- 美多商城学习测试/美多/book/views.py
+++ 美多商城学习测试/美多/book/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
 # Create your views here.
+"""
+视图
+视图函数 即python的函数
+要求：
+1.第一个参数接受请求
+2.
+"""
+from django.http import HttpRequest
+from django.http import HttpResponse
+#我们期望用户输入 http://127.0.0.1:8000/index/
+#来访问视图函数
+def index(request):
+    return HttpResponse("OK")

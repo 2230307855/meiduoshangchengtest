035519b6ad2543b8ed99108bcc33e1222130b3fd
Chenglong Shi <2230307855@qq.com>
2021-11-05 00:34:40 +0800

    出错后第一次路由成功

--- 美多商城学习测试/美多/book/views.py
+++ 美多商城学习测试/美多/book/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+#视图函数有两个要求
+#请求 第一个参数
+#相应 第二个参数
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse("ok")

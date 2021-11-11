15aeab7ed897fe02713a05e1b461a31857eff61a
Chenglong Shi <2230307855@qq.com>
2021-11-11 10:51:16 +0800

    数据库的查询

--- 美多商城学习测试/美多2/book/views.py
+++ 美多商城学习测试/美多2/book/views.py
@@ -45,3 +45,46 @@ book.delete()
 #2.查询删除
 # BookInfo.objects.get(id=6).delete()
 BookInfo.objects.filter(id=6).delete()
+####################################################
+#查询
+#get （单一对象）（精确查询）
+# try:
+#     book=BookInfo.objects.get(id=1)
+# except BookInfo.DoesNotExist:
+#     print('查询的结果不存在')
+# #
+#all
+# books=BookInfo.objects.all()
+#获取所有的人数
+from book.models import PeopleInfo
+# peoples=PeopleInfo.objects.all()
+renshu=PeopleInfo.objects.all().count()
+#filter(过滤查询)
+#联合使用的函数：
+# filter过滤出多个结果 exclude排除掉符合条件
+# get过滤获取单一的结果
+
+#模型类名.objects.filter(属性名__运算符=值) n个结果
+#模型类名.objects.exclude(属性名__运算符=值) n个结果
+#模型类名.objects.get(属性名__运算符=值)  1个结果或者异常
+
+#查询编号为1的图书
+book=BookInfo.objects.get(id=1)
+BookInfo.objects.get(pk=1)#主键获取对象
+#查询书名中包含湖的书
+BookInfo.objects.filter(name__contains='湖')
+#查询书名以部结尾的图书
+BookInfo.objects.filter(name__endswith='部')
+#查询书名为空的图书
+BookInfo.objects.filter(name__isnull=True)
+#查询编号为1|2|5的图书
+BookInfo.objects.filter(id__in=[1,3,5])
+#查询编号大于3的书
+#gt lt gte lte
+BookInfo.objects.filter(id__gt=3)
+#查询编号不等于3的书籍 exclude 排除id=3的书
+BookInfo.objects.exclude(id=3)
+#查询1980年发表的图书
+BookInfo.objects.filter(pub_date__year=1980)
+#查询1990年1月1日后发表的图书
+BookInfo.objects.filter(pub_date__gt='1990-1-1')

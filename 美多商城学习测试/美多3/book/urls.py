098dfd4ab3a86516d38e281d21bc140226762185
Chenglong Shi <2230307855@qq.com>
2021-11-19 23:46:01 +0800

    请求路径中的参数与查询字符串

--- 美多商城学习测试/美多3/book/urls.py
+++ 美多商城学习测试/美多3/book/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
-from book.views import create_book
+from book.views import create_book,shop
 urlpatterns={
     path('create/',create_book),
+    # #一行的路由
+    # path('11000/11005/',shop),
+    #占位符来 代表大量的路由索引
+    path('<city_id>/<shop_id>/',shop),
 }
--- 美多商城学习测试/美多3/book/views.py
+++ 美多商城学习测试/美多3/book/views.py
@@ -9,3 +9,27 @@ def create_book(request):
         readcount=10
     )
     return HttpResponse('create')
+
+def shop(request,city_id,shop_id):
+    # print('city_id is:',city_id,' shop_id is:',shop_id)
+    query_params=request.GET
+    print(query_params)
+    # order=query_params.get('order')
+    # order=query_params['order']#如果多个值，选择最后赋予的值
+    #如果要的到所有的值，要用 order=query_params.getlist('order')
+    # print(order)
+    return HttpResponse('我的饭店')
+#<QueryDict: {'order': ['readcount', 'commentcount'], 'page': ['1']}>
+#QueryDict 具有字典的特性
+#可以一键多指
+
+
+###############################################
+"""
+查询字符串
+http://ip:port/path/path/?key=value&key1=value1
+url 以 ？ 来进行分割  分为2部分
+？前边为 请求路径
+？后边为 查询的字符串  类似于字典key=value 多个用&连接
+
+"""
